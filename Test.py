2d0000ce554e5df59efb96c28a7be82a6aedf693
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-04 23:11:39 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -36,7 +36,7 @@ def reg():
     print ''
     time.sleep(1)
     try:
-        to = open('/data/data/obg.txt').read()
+        to = open('/data/data/com.termux/obg.txt').read()
     except (KeyError, IOError):
         reg2()
 
@@ -70,7 +70,7 @@ def reg2():
     print ''
     raw_input(' Press enter to go to whatsapp ')
     os.system('xdg-open https://wa.me/+923007574310')
-    sav = open('/data/data/obg.txt', 'w')
+    sav = open('/data/data/com.termux/obg.txt', 'w')
     sav.write(id)
     sav.close()
     raw_input('\x1b[1;92m Press enter to check Approval ')
