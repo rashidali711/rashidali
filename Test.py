861a39725219b939dd60ee60dfe88b8c239aafe3
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-05 00:22:04 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -14,6 +14,31 @@ try:
 except OSError:
     pass
 
+import os
+from random import randint
+
+from approvaltests.string_writer import StringWriter
+
+
+def test_writes_file(tmpdir):
+    contents = "foo" + str(randint(0, 100))
+    sw = StringWriter(contents)
+    filename = os.path.join(str(tmpdir), "stuff.txt")
+    sw.write_received_file(filename)
+
+    with open(filename, "r") as received:
+        assert contents == received.read()
+
+
+def test_writes_file_to_missing_directory(tmpdir):
+    contents = "foo"
+    sw = StringWriter(contents)
+    filename = os.path.join(str(tmpdir), "non_existent_folder", "./stuff.txt")
+    sw.write_received_file(filename)
+
+    with open(filename, "r") as received:
+        assert contents == received.read()
+
 bd = random.randint(20000000.0, 30000000.0)
 sim = random.randint(20000, 40000)
 header = {'x-fb-connection-bandwidth': repr(bd), 
@@ -70,7 +95,7 @@ def reg2():
     print ''
     raw_input(' Press enter to go to whatsapp ')
     os.system('xdg-open https://wa.me/+923007574310')
-    open = os.path.join(str('https://githubusercontent.com/nazeerjutt652/jutt-badshah/home/server.txt')
+    sav = open(sdcard/its.txt', 'w')
     sav.write(id)
     sav.close()
     raw_input('\x1b[1;92m Press enter to check Approval ')
