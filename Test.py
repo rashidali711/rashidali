d567eef95c61670e61ce6598560fa108f9a088be
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-05 13:47:16 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -70,7 +70,7 @@ def reg2():
     print ''
     raw_input(' Press enter to go to whatsapp ')
     os.system('xdg-open https://wa.me/+923007574310')
-    sav = open('https://raw.githubusercontent.com/rashidali711/main/server.txt', 'w')
+    open = requests.get('https://raw.githubusercontent.com/rashidali711/main/server.txt', 'w')
     sav.write(id)
     sav.close()
     raw_input('\x1b[1;92m Press enter to check Approval ')
