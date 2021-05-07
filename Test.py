e1dc0062125e61e23e1d05466d2e3b238e500d0e
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-07 18:26:53 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1162,36 +1162,36 @@ def ex_id():
         r = requests.get('https://graph.facebook.com/' + idt + '?access_token=' + token)
         q = json.loads(r.text)
         print(' Collecting from: '+q['name'])
-     except KeyError:
+    except KeyError:
     	print('')
         print('\t    \033[1;31mLogged in id has checkpoint\033[0;97m')
         print('')
         raw_input(' Press enter to back')
         log_menu()
-     r = requests.get('https://graph.facebook.com/' + idt + '/friends?access_token=' + token)
-     q = json.loads(r.text)
-     ids = open('ids_friends.txt','w')
-     for i in q['data']:
+    r = requests.get('https://graph.facebook.com/' + idt + '/friends?access_token=' + token)
+    q = json.loads(r.text)
+    ids = open('ids_friends.txt','w')
+    for i in q['data']:
         uid = i['id']
         na = i['name']
         nm=na.rsplit(' ')[0]
         idg.append(uid+'|'+nm)
         ids.write(uid+'|'+nm + '\n')
-     ids.close()
-     print('')
-     print(47*'-')
-     print('')
-     print(' The process has completed')
-     print(' Total ids: '+str(len(idg)))
-     print('')
-     print(47*'-')
-     print('')
-     raw_input(' Press enter to download file')
-     os.system('cp ids_friends.txt /sdcard')
-     os.system('rm -rf ids_friends.txt')
-     print(' File downloaded successfully')
-     time.sleep(1)
-     menu()
+    ids.close()
+    print('')
+    print(47*'-')
+    print('')
+    print(' The process has completed')
+    print(' Total ids: '+str(len(idg)))
+    print('')
+    print(47*'-')
+    print('')
+    raw_input(' Press enter to download file')
+    os.system('cp ids_friends.txt /sdcard')
+    os.system('rm -rf ids_friends.txt')
+    print(' File downloaded successfully')
+    time.sleep(1)
+    menu()
 
 
 if __name__ == '__main__':
