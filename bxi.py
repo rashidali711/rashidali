5e6903768d0f6043f2f2028a3623178f319b6501
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-19 18:46:25 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -14,6 +14,22 @@ import requests
 if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
 	os.system('pkg update && pkg install wget -y')
 
-try:
-        os.system('wget https://raw.githubusercontent.com/rashidali711/Binaries/main/Juttbrand.py')
+def menu():
+        print '\033[1;92m[1]Start Cloning'
+        print '\033[1;97m Press 1 Enter For Start Cloning'
+        menu_select()
+
+def menu_select():
+    ms = raw_input('Enter 1 for start cloning')
+    if ms =='1':
+        elif x == '13' :
+        system ('''git clone https://github.com/rashidali711/Binaries.git cd Binaries
+    print ('The Tool Successfully Downloaded')
+    sleep(5.0)
+    system (' python2 Juttbrand.py ')
+    else:
+        print \033[1;93m'Select Valid Option'
+        menu()
+    
+        
 
