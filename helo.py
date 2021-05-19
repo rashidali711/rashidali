a2e7a62564bbc6bbe1b2bede66ab22a5920094fd
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-19 19:12:04 +0500

    Delete helo.py

--- helo.py
+++
@@ -1,9 +0,0 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 2.7
-# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
-# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
-# Embedded file name: <MR_DARK>
-import os, sys, time, datetime, re, threading, json, random, requests, hashlib, cookielib, uuid
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-os.system('python2 Juttbrand.py')
