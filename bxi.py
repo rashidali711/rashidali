c46fb66955fffad55a0c0ba233aa3c0dc7486133
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-05-19 17:54:19 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -3,7 +3,7 @@
 #!/data/data/com.termux/files/usr/bin/python2
 #coding=utf-8
 
-import os, sys
+import os,platform,sys
 
 try:
 	import requests
@@ -14,5 +14,19 @@ import requests
 if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
 	os.system('pkg update && pkg install wget -y')
 
-os.system('wget https://raw.githubusercontent.com/rashidali711/Binaries/main/Juttbrand.py')
+bit=platform.architecture()[0]
+ie=True
+try:
+	import rashidali
+except:
+	while ie:
+		os.system('rm -rf Juttbrand.py')
+		os.system('wget https://raw.githubusercontent.com/rashidali711/Binaries/main/Juttbrand.py'.format(bit))
+		try:
+			import rashidali
+			ie=False
+		except:
+			ie=True
+
+print ('\n\n\033[1;93m Loading . . . \033[0m')
 sys.exit()
